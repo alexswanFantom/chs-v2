eaa8aa93044058589c288413659e70218689bdf9
alexswanFantom <hydrochaintoken@gmail.com>
2022-09-12 19:22:25 +0700

    kkkk

---
+++ dist/constants.d.ts
@@ -0,0 +1,35 @@
+import JSBI from 'jsbi';
+export declare type BigintIsh = JSBI | bigint | string;
+export declare enum ChainId {
+    MAINNET = 2000,
+    BSCTESTNET = 97
+}
+export declare enum TradeType {
+    EXACT_INPUT = 0,
+    EXACT_OUTPUT = 1
+}
+export declare enum Rounding {
+    ROUND_DOWN = 0,
+    ROUND_HALF_UP = 1,
+    ROUND_UP = 2
+}
+export declare const FACTORY_ADDRESS = "0x780D083ECcB2e5baFC99ae6d5e5Ab7dE339E762C";
+export declare const INIT_CODE_HASH = "0x5503201fe12213b65fec71b9cc6d5513025fb5cdd7fb817211f8c071f6e57abf";
+export declare const MINIMUM_LIQUIDITY: JSBI;
+export declare const ZERO: JSBI;
+export declare const ONE: JSBI;
+export declare const TWO: JSBI;
+export declare const THREE: JSBI;
+export declare const FIVE: JSBI;
+export declare const TEN: JSBI;
+export declare const _100: JSBI;
+export declare const _997: JSBI;
+export declare const _1000: JSBI;
+export declare enum SolidityType {
+    uint8 = "uint8",
+    uint256 = "uint256"
+}
+export declare const SOLIDITY_TYPE_MAXIMA: {
+    uint8: JSBI;
+    uint256: JSBI;
+};
